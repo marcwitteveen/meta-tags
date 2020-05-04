0f4beae36c5e6f878e68ac5a53c6e12fb6eac625
Marc Witteveen <marc.witteveen@gmail.com>
2020-05-04 10:54:58 +0200

    First commit of file and folder structure :

---
+++ src/MetaTags.php
@@ -0,0 +1,65 @@
+<?php 
+
+/**
+ * This file is part of the MetaTags package.
+ * 
+ * (c) Marc Witteveen <marc.witteveen@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MarcWitteveen\MetaTags;
+
+class MetaTags {
+
+	private $meta;
+
+	private $rules = [];
+
+	private $aliases = [];
+
+	public final function __construct()
+	{
+		$this->init();
+	}
+
+	protected function init() 
+	{
+		$this
+			->addRule('title', [$this, 'ruleTitle']);
+	}
+
+	protected final function getMeta()
+    {
+        return $this->meta;
+    }
+
+	protected function ruleTitle($value)
+    {
+    	$this->getMeta()->addChild('title', $value);
+    }
+
+	protected final function addAlias($alias, $name)
+    {
+        $this->aliases[$alias] = $name;
+		return $this;
+    }
+
+	protected final function addRule($name, callable $callable)
+	{
+		$this->rules[$name] = $callable;
+		return $this;
+	}
+
+	public final function add($name, $value)
+	{
+
+	}
+
+	public final function build()
+	{
+		$build = '';
+		return $build;
+	}
+}
